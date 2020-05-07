f3f12e057f21c6a8f1dd7d4d5c47f78108d6cbff
Cholado <sntgmtnz@gmail.com>
2020-05-07 23:21:01 +0000

    fixed - max func took number value of the ascii or unicode

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary == {}:
-        return
-    n = []
-    for new in a_dictionary:
-        n.append(new)
-    return max(n)
+    if a_dictionary is None:
+        return None
+
+    best = max(a_dictionary.values(), default=None)
+    for key, value in a_dictionary.items():
+        if value == best:
+            return key
